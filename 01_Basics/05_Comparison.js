d676d01574b6c30950e4d18b7b0b2ddc12415ed3
Sudarshan Shelke <166682319+sudarshanshelke25@users.noreply.github.com>
2025-07-15 15:05:26 +0000

    Add 05_Comparison.js with examples of comparison operations in JavaScript

---
+++ 01_Basics/05_Comparison.js
@@ -0,0 +1,36 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("\n");
+
+console.log("2" > 1);
+console.log("02" >= 1);
+console.log("2" < 1);
+console.log("2" <= 1);
+console.log("2" == 1);
+console.log("2" != 1);
+console.log("\n");
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log("\n");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log("\n");
+
+console.log("" == 0);
+console.log("" > 0);
+console.log("" < 0);
+console.log("\n");
+
+console.log("2" == 2);
+console.log("2" > 2);
+console.log("2" < 2);
+console.log("2" === 2);
+console.log("\n");
