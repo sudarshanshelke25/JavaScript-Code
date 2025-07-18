d5afb2b559b889948a35d31b32ad0ebdce97ba27
Sudarshan Shelke <166682319+sudarshanshelke25@users.noreply.github.com>
2025-07-18 13:15:43 +0000

    Add 02_Switch.js to demonstrate switch statements in JavaScript

---
+++ 04_Conditionals/02_Switch.js
@@ -0,0 +1,104 @@
+// Switch Statements :
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Invalid month");
+        break;
+}
+
+const monthShort = "jan";
+
+switch (monthShort.toLowerCase()) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "jun":
+        console.log("June");
+        break;
+    case "jul":
+        console.log("July");
+        break;
+    case "aug":
+        console.log("August");
+        break;
+    case "sep":
+        console.log("September");
+        break;
+    case "oct":
+        console.log("October");
+        break;
+    case "nov":
+        console.log("November");
+        break;
+    case "dec":
+        console.log("December");
+        break;
+
+    default:
+        console.log("Invalid month");
+        break;
+}
+
+// Truthy value or Falsy Value :
+
