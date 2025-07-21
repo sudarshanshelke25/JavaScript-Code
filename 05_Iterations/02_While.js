499c587e68765f7f4592e24dcbb21fd217eba7a1
Sudarshan Shelke <166682319+sudarshanshelke25@users.noreply.github.com>
2025-07-21 07:52:04 +0000

    Add 02_While.js to demonstrate While and Do While Loop and its application in JavaScript

---
+++ 05_Iterations/02_While.js
@@ -0,0 +1,49 @@
+// While Loop :
+
+// while (condition) {
+    
+// }
+let i = 0
+while (i <=10 ) {
+    console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+console.log("\n");
+
+let j = 11
+while (j <=10 ) {
+    console.log(`Value of i is ${j}`);
+    j = j + 2;
+}
+console.log("\n");
+
+let myArray = ["spider man", "hulk", "iron man", "thor", "loki", "black widow", "caption marvel", "caption america"]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr = arr + 1;
+}
+console.log("\n");
+
+
+// Do while Loop :
+
+// do {
+    
+// } while (condition);
+
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <=10);
+console.log("\n");
+
+let score1 = 11;
+
+do {
+    console.log(`Score is ${score1}`);
+    score1++;
+} while (score1 <=10);
+console.log("\n");
